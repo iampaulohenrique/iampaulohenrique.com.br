d788967f0cdd2d456880641c846fc4cbd655d6bb
paulo henrique <phlpaulohenriqueluiz@gmail.com>
2024-09-06 17:15:37 -0300

    mudança na estrutura

---
+++ site/src/App.js
@@ -0,0 +1,65 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        
+        
+        <h1>Seja vem vindo!</h1>
+        <h2>Sobre</h2>
+        <p>Tenho 24 anos e sou estudante de Ciência da Computação. Minha paixão por tecnologia e programação é o que me impulsiona a explorar novas ideias e soluções inovadoras no campo da informática. 💻✨</p>
+        <p>Neste site, você encontrará informações sobre o meu trabalho, projetos em que estou envolvido e as áreas que mais me fascinam. 🚀🔍 É também um ponto de contato para futuras colaborações e projetos interessantes. 💼🤝</p>
+        <p>Sinta-se à vontade para explorar, conhecer mais sobre o que faço e entrar em contato para qualquer dúvida ou proposta. Estou sempre aberto a novas oportunidades e desafios! </p>
+        <p>Obrigado por visitar, e espero que você encontre algo inspirador por aqui!</p>
+        <p>Atenciosamente, Paulo Henrique 👨‍💻</p>
+        
+        <a
+          className="App-link"
+          href="https://www.instagram.com/iampaulohen"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Instagram
+        </a>
+        <a
+          className="discord"
+          href="https://discord.gg/c9SwrXDqMK"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Discord
+        </a>
+        <a
+        className="Github"
+        href="https://github.com/iampaulohenrique"
+        target="_blank"
+        rel="noopener noreferrer"
+        >
+          Github
+        </a>
+        <a
+        className="linkedin"
+        href="https://www.linkedin.com/in/iampaulohenrique?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app "
+        target="_blank"
+        rel="noopener noreferrer"
+        >
+          Linkedin
+        </a>
+        <a 
+        className="certificados"
+        href="https://drive.google.com/drive/folders/1yBLvbexCsdAmRVmvDHL3gXczzR8dEzQB?usp=sharing"
+        target="_blank"
+        rel="noopener noreferrer"
+        >
+          Certificados
+        </a>
+      </header>
+    
+    </div>
+  );
+}
+
+export default App;
