de83434c2f04085d1ec1f04c2d37dc8bfec30bd3
Paulo Henrique <phlpaulohenriqueluiz@gmail.com>
2024-09-09 11:40:24 -0300

    adicionado alguns efeitos front-end ao site

--- site/src/App.js
+++ site/src/App.js
@@ -1,33 +1,33 @@
-import logo from './logo.svg';
+import logo from './imagens/logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div className="image-container">
+          <img src={logo} alt="uma imagem em desenho do Paulo Henrique" className="image-hover-effect" />
+        </div>
         
-        
-        <h1>Seja vem vindo!</h1>
+        <h1>Seja bem-vindo!</h1>
         <p>Tenho 24 anos e sou estudante de Ciência da Computação. Minha paixão por tecnologia e programação é o que me impulsiona a explorar novas ideias e soluções inovadoras no campo da informática. 💻✨</p>
         <p>Neste site, você encontrará informações sobre o meu trabalho, projetos em que estou envolvido e as áreas que mais me fascinam. 🚀🔍 É também um ponto de contato para futuras colaborações e projetos interessantes. 💼🤝</p>
-        <p>Sinta-se à vontade para explorar, conhecer mais sobre o que faço e entrar em contato para qualquer dúvida ou proposta. Estou sempre aberto a novas oportunidades e desafios! </p>
+        <p>Sinta-se à vontade para explorar, conhecer mais sobre o que faço e entrar em contato para qualquer dúvida ou proposta. Estou sempre aberto a novas oportunidades e desafios!</p>
         <p>Obrigado por visitar, e espero que você encontre algo inspirador por aqui!</p>
         <p>Atenciosamente, Paulo Henrique 👨‍💻</p>
         
-        <div class="icon-container">
-    <a href="https://www.instagram.com/iampaulohen" class="App-link" target="_blank" rel="noopener noreferrer" >Instagram</a>
-    <a href="https://github.com/iampaulohenrique" class="Github" target="_blank" rel="noopener noreferrer" >Github</a>
-    <a href="https://discord.gg/c9SwrXDqMK" class="discord" target="_blank" rel="noopener noreferrer" >Discord</a>
-    <a href="https://www.linkedin.com/in/iampaulohenrique" class="linkedin"target="_blank" rel="noopener noreferrer" >Linkedin</a>
-    <a href="https://drive.google.com/drive/folders/1yBLvbexCsdAmRVmvDHL3gXczzR8dEzQB" class="certificados" target="_blank" rel="noopener noreferrer">Certificados</a>
-      </div>
+        <div className="icon-container">
+          <a href="https://www.instagram.com/iampaulohen" className="App-link" target="_blank" rel="noopener noreferrer">Instagram</a>
+          <a href="https://github.com/iampaulohenrique" className="Github" target="_blank" rel="noopener noreferrer">Github</a>
+          <a href="https://discord.gg/c9SwrXDqMK" className="discord" target="_blank" rel="noopener noreferrer">Discord</a>
+          <a href="https://www.linkedin.com/in/iampaulohenrique" className="linkedin" target="_blank" rel="noopener noreferrer">Linkedin</a>
+          <a href="https://drive.google.com/drive/folders/1yBLvbexCsdAmRVmvDHL3gXczzR8dEzQB" className="certificados" target="_blank" rel="noopener noreferrer">Certificados</a>
+        </div>
       </header>
-      <footer class="footer">
-      Copyright © 2024. Todos os direitos reservados
-  </footer>
-</div>
-    
+      <footer className="footer">
+        Copyright © 2024. Todos os direitos reservados
+      </footer>
+    </div>
   );
 }
 
